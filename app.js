e399e41338a479d2c9fd15453579f0759e3458be
Juan Angarita <juandiegoangarita246800@gmail.com>
2025-02-01 19:39:49 -0500

    Capturar el valor del campo de entrada

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-let amigos = ['Juan', 'Pepe', 'Luis', 'Ana', 'Maria'];
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function agregarAmigo(){
+    const inputAmigo = document.getElementById('Amigo')
+    const nombre = inputAmigo.ariaValueMax.trim();
+}
